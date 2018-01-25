ccfed0c8809a1210586f3b6f150cefcd1e027d41
louisoft01 <louisoft01@gmail.com>
2018-01-25 10:08:40 +0000

    Add files via upload

---
+++ Pages/FAQ.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Louisoft01.com.Pages
+{
+    public class FAQ : PageModel
+    {
+        public FAQ(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "Your contact page.";
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Louisoft01.com.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
